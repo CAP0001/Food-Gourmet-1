78ed5533e0fda9dda7deb5c8ceb1ddb12547bc5f
CAP0001 <yarokovalj22@gmail.com>
2026-03-08 17:27:32 +0200

    media and staff

--- restourant_app/forms.py
+++ restourant_app/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Profile 
 
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput)
+
 class UpdateProfileForm(forms.ModelForm):
     avatar = forms.ImageField(required=False)
 
